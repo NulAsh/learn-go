ec69d54591d66e5d587ef47db3eccfdb6f14e44b
NulAsh <32499449+NulAsh@users.noreply.github.com>
2019-10-25 20:36:55 +0300

    Update server.go

--- server.go
+++ server.go
@@ -11,7 +11,68 @@ import (
     "path/filepath"
 )
 
-const BUFFERSIZE = 1024
+func listDir(curDir string, conn net.Conn) (err error) {
+    files, err := ioutil.ReadDir(curDir)
+    if err != nil {
+        return
+    }
+    conn.Write([]byte(curDir))
+    conn.Write([]byte(fmt.Sprintf("\n%d\n", len(files))))
+    for _, file := range files {
+        if file.IsDir() {
+            conn.Write([]byte("D " + file.Name() + "\n"))
+        } else {
+            conn.Write([]byte(fmt.Sprintf("%d", file.Size()) + " " + file.Name() + "\n"))
+        }
+        fmt.Println(file.Name())
+    }
+    return
+}
+
+func downloadFile(filename string, conn net.Conn) (err error){
+    file, err := os.Open(filename)
+    if err != nil {
+        return
+    }
+    defer file.Close()
+    fileinfo, err := os.Stat(filename)
+    if err != nil {
+        return
+    }
+    conn.Write([]byte(fmt.Sprintf("%d\n", fileinfo.Size())))
+    wbytes, err := io.Copy(conn, file)
+    if err != nil {
+        return
+    }
+    if wbytes != fileinfo.Size() {
+        fmt.Printf("%d bytes of %d written\n", wbytes, fileinfo.Size())
+    }
+    return
+}
+
+func uploadFile(filename string, conn net.Conn) (err error){
+    file, err := os.Create(filename)
+    if err != nil {
+        return
+    }
+    defer file.Close()
+    filelenStr, err := bufio.NewReader(conn).ReadString('\n')
+    if err != nil {
+        return
+    }
+    filelen, err := strconv.ParseInt(strings.TrimSpace(filelenStr), 10, 64)
+    if err != nil {
+        return
+    }
+    rbytes, err := io.CopyN(file, conn, filelen)
+    if err != nil {
+        return
+    }
+    if rbytes != filelen {
+        fmt.Printf("%d bytes of %d read\n", rbytes, filelen)
+    }
+    return
+}
 
 func handleConnection(conn net.Conn) {
     defer conn.Close()
@@ -34,69 +95,28 @@ func handleConnection(conn net.Conn) {
         switch message[0] {
         case 'L':
             // Listing of the current directory
-            files, err := ioutil.ReadDir(curDir)
+            err := listDir(curDir, conn)
             if err != nil {
                 fmt.Println("Error: " + err.Error())
                 return
             }
-            conn.Write([]byte(curDir))
-            conn.Write([]byte(fmt.Sprintf("\n%d\n", len(files))))
-            for _, file := range files {
-                conn.Write([]byte(fmt.Sprintf("%d", file.Size()) + " " + file.Name() + "\n"))
-                fmt.Println(file.Name())
-            }
         case 'D':
             //
             filename := filepath.Join(curDir, filepath.Base(strings.TrimSpace(message[1:])))
-            file, err := os.Open(filename)
+            err := downloadFile(filename, conn)
             if err != nil {
                 fmt.Println("Error " + err.Error())
                 conn.Write([]byte("Error " + err.Error() + "\n"))
-            } else {
-                defer file.Close()
-                fileinfo, err := os.Stat(filename)
-                if err != nil {
-                    fmt.Println("Error " + err.Error())
-                    conn.Write([]byte("Error " + err.Error() + "\n"))
-                } else {
-                    conn.Write([]byte(fmt.Sprintf("%d\n", fileinfo.Size())))
-                    wbytes, err := io.Copy(conn, file)
-                    if err != nil {
-                        fmt.Println("Error " + err.Error())
-                        conn.Write([]byte("Error " + err.Error() + "\n"))
-                    } else if wbytes != fileinfo.Size() {
-                        fmt.Printf("%d bytes of %d written\n", wbytes, fileinfo.Size())
-                    }
-                }
+                return
             }
         case 'U':
             //
             filename := filepath.Join(curDir, filepath.Base(strings.TrimSpace(message[1:])))
-            file, err := os.Create(filename)
+            err := uploadFile(filename, conn)
             if err != nil {
                 fmt.Println("Error " + err.Error())
                 conn.Write([]byte("Error " + err.Error() + "\n"))
-            } else {
-                defer file.Close()
-                filelenStr, err := bufio.NewReader(conn).ReadString('\n')
-                if err != nil {
-                    fmt.Println("Error " + err.Error())
-                    conn.Write([]byte("Error " + err.Error() + "\n"))
-                } else {
-                    filelen, err := strconv.ParseInt(strings.TrimSpace(filelenStr), 10, 64)
-                    if err != nil {
-                        fmt.Println("Error " + err.Error())
-                        conn.Write([]byte("Error " + err.Error() + "\n"))
-                    } else {
-                        rbytes, err := io.CopyN(file, conn, filelen)
-                        if err != nil {
-                            fmt.Println("Error " + err.Error())
-                            conn.Write([]byte("Error " + err.Error() + "\n"))
-                        } else if rbytes != filelen {
-                            fmt.Printf("%d bytes of %d read\n", rbytes, filelen)
-                        }
-                    }
-                }
+                return
             }
         }
         fmt.Printf("%#v\n", message)
